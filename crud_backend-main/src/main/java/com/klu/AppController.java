9813c145cc09a75945069cebbbb89ae9b6af1251
VandanaTungala <vandanatungala889@gmail.com>
2025-11-04 01:26:30 +0530

    Initial commit - Spring Boot backend

---
+++ crud_backend-main/src/main/java/com/klu/AppController.java
@@ -0,0 +1,36 @@
+package com.klu;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5174") // 👈 add frontend URL here
+@RequestMapping("/springapp1") // 👈 optional but helps structure URLs
+public class AppController {
+
+    @Autowired
+    Service s;
+
+    @PostMapping("/insert")
+    public String insert(@RequestBody Product p) {
+        return s.insertData(p);
+    }
+
+    @GetMapping("/display")
+    public List<Product> display() {
+        return s.displayData();
+    }
+
+    @PutMapping("/update")
+    public String update(@RequestBody Product p) {
+        return s.updateData(p);
+    }
+
+    /*
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable int id) {
+        return s.deleteData(id);
+    }
+    */
+}
